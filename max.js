0ad524446e7b7d1592e235a3d474ae5972d3b5d8
user <user>
2016-08-04 20:45:08 +0800

    fb login

--- max.js
+++ max.js
@@ -1,7 +1,7 @@
 var request = require('request');
-var calc = require('./calc.js');
 var express = require('express');
 var fs = require('fs');
+var cookie = require('cookie-parser');
 
 var webPage ={
 	html:''
@@ -27,13 +27,13 @@ fs.readFile('./max.github.io/maxjs.js','UTF-8',function(err,data){
 		webPage.js=data;
 		} else console.log(err);
 });
-fs.readFile('./max.github.io/jpg.jpg',function(err,data){
+/*fs.readFile('./max.github.io/jpg.jpg',function(err,data){
 	if(!err){
 		webPage.img=data;
 		} else console.log(err);
-});
+});*/
 
-//pp.use('/',express.static(__dirname+'/max.github.io'));
+app.use('/',express.static(__dirname+'/max.github.io'));
 
 app.get('/',function(req,res){
 	res.send(webPage.html);
@@ -49,11 +49,11 @@ app.get('/maxjs.js',function(req,res){
 	res.write(webPage.js);
 	res.end();
 });
-app.get('/maxjs.js',function(req,res){
+/*app.get('/maxjs.js',function(req,res){
 	res.set('Content-Type', 'image/jpeg');
 	res.write(webPage.img);
 	res.end();
-});
+});*/
 app.get('/in/:x',function(req,res){
 	res.json({
 		'data1': 3,
